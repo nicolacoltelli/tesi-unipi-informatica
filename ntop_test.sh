7d7f9352e827b89a8aa828727794962112d64d76
nicolacoltelli <coltellinicola7@gmail.com>
2021-10-19 12:41:04 +0200

    add script to test with ntop data

---
+++ ntop_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+folder="/var/lib/ntopng/2/rrd/"
+
+python3 des.py --input $folder > out.txt
+
+grep "anomaly found" out.txt > anomaly_found.txt
+grep "Correlation found" out.txt > correlation_found.txt
+grep "continuous" out.txt > continuous.txt
+grep "debug" out.txt > debug.txt
+
+rm out.txt
